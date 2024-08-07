bf16fd0c18e4f9335db5dfbc33c2017c5355c68e
Emir <bemir5795@gmail.com>
2024-08-07 11:35:52 +0300

    index js edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-
 document.addEventListener("DOMContentLoaded", function() {
+    loadHTML('navbar-container', 'components/navbar.html');
+    loadHTML('footer-container', 'components/footer.html');
+    loadHTML('announcements-container', 'components/announcements.html');
+    loadHTML('home-container', 'components/home.html', initializeSlides);
+});
+
+function loadHTML(elementId, url, callback) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (callback) callback();
+        });
+}
+
+function initializeSlides() {
     let slideIndex = 0;
     showSlides();
 
@@ -7,18 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
         let i;
         let slides = document.getElementsByClassName("mySlides");
 
-        if (slideIndex >= slides.length) {slideIndex = 0}    
-        if (slideIndex < 0) {slideIndex = slides.length - 1}
+        if (slideIndex >= slides.length) { slideIndex = 0 }
+        if (slideIndex < 0) { slideIndex = slides.length - 1 }
 
         for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
+            slides[i].style.display = "none";
         }
 
-        slides[slideIndex].style.display = "block";  
+        slides[slideIndex].style.display = "block";
 
         slideIndex++;
-        if (slideIndex >= slides.length) {slideIndex = 0}
+        if (slideIndex >= slides.length) { slideIndex = 0 }
 
-        setTimeout(showSlides, 3000); 
+        setTimeout(showSlides, 3000);
     }
-});
+}
